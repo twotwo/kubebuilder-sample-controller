b0f9a3b8ab154d4ddcb0db9d66c32b51c122c32c
liyan <twotwo.li@gmail.com>
2025-08-13 14:54:20 +0800

    ci: fix e2e test & only publish dev as latest

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -39,7 +39,7 @@ var (
 
 	// projectImage is the name of the image which will be build and loaded
 	// with the code source changes to be tested.
-	projectImage = "example.com/kubebuilder-sample-controller:v0.0.1"
+	projectImage = "twotwo/pod-billing-controller"
 )
 
 // TestE2E runs the end-to-end (e2e) test suite for the project. These tests execute in an isolated,
